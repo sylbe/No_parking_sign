a56188a19a0b0c19dcfcf2a1b2d65d9db8afdb30
Sylvain BELLIER <sylvain.bellier@orange.fr>
2016-09-09 08:33:14 +0200

    commit

--- No_parking_sign.ino
+++ No_parking_sign.ino
@@ -47,14 +47,18 @@ myleds.attach(myledPins,10);
 // The loop function is called in an endless loop
 void loop()
 {
-  //Add your repeated code here
+  int i;
+	//Add your repeated code here
 	 //myleds.animateLeds(1000, FIXED_ON,0); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
 
 	//otherleds.animateLeds(1000, CHASE_CYCLE, 1); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
 	//otherleds.animateLeds(1000, BLINKING, 50); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
 
 	//myleds.animateLeds(3000, BLINKING, 30); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
-	myleds.animateLeds(1000, CHASE_CYCLE, 9); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
-
+	//myleds.animateLeds(1000, CHASE_CYCLE, 9); //I want all the Leds blinking 50% on 50% off with a period of 1000ms (1s)
+	for (i=0; i<=100;i++) {
+	myleds.animateLeds(100, VUMETER, i); //vumeter 50%
+	delay (100);
+	}
 }
 
